59c25879670e3fa2413e700c2f29796b77b218bd
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-04-15 09:26:23 -0700

    feat: Adds Lombok in pom.xl

--- src/main/java/dev/java10x/NinjaSignUp/Missions/Service/MissionModel.java
+++ src/main/java/dev/java10x/NinjaSignUp/Missions/Service/MissionModel.java
@@ -2,45 +2,29 @@ package dev.java10x.NinjaSignUp.Missions.Service;
 
 import dev.java10x.NinjaSignUp.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_mission")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
 public class MissionModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private Rank rank;
     // A mission can be applied to more than one ninja
     @OneToMany(mappedBy = "missions")
     private List<NinjaModel> ninja;
-
-    public MissionModel() {
-    }
-
-    public MissionModel(Long id, String name, Rank rank) {
-        this.id = id;
-        this.name = name;
-        this.rank = rank;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Rank getRank() {
-        return rank;
-    }
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
-    }
 
 }
--- src/main/java/dev/java10x/NinjaSignUp/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/NinjaSignUp/Ninjas/NinjaModel.java
@@ -2,13 +2,21 @@ package dev.java10x.NinjaSignUp.Ninjas;
 
 import dev.java10x.NinjaSignUp.Missions.Service.MissionModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // Entity Transforms a class to an entity in DB
 @Entity
 @Table(name = "tb_sign_up")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class NinjaModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,44 +26,8 @@ public class NinjaModel {
     private String email;
 
     private int age;
-
     // A ninja has only one mission
     @ManyToOne
     @JoinColumn(name = "mission_id") // Foregnein key ( I have no idea on how to write this)
     private MissionModel missions;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
