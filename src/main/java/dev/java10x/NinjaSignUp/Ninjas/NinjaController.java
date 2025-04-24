676893e701c1d6c4762fa4d4bda021151918f6bc
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-04-24 09:41:22 -0700

    feat: adds shell crud operations to /NinjaController

--- src/main/java/dev/java10x/NinjaSignUp/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/NinjaSignUp/Ninjas/NinjaController.java
@@ -3,12 +3,44 @@ package dev.java10x.NinjaSignUp.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome";
 
     }
+
+    // Add Ninja (Create)
+    @PostMapping("/create")
+    public String createNinja(){
+        return "Ninja Created";
+    }
+
+    // Show ninjas (Read)
+    @GetMapping("/readall")
+    public String showAllNinjas(){
+        return "Shown all Ninjas";
+    }
+
+    // Show ninja by ID (Read)
+    @GetMapping("/read")
+    public String showNinjaByID(){
+        return "Ninja shown by ID";
+    }
+
+    // Change data  (Update)
+    @PutMapping("/updateID")
+    public String updateNinjaByID(){
+        return "Ninja change by ID";
+    }
+
+    // Delete Ninja (Delete)
+    @DeleteMapping("/deleteID")
+    public String deleteID(){
+        return "Ninja deleted";
+    }
+
+    // All of this is called CRUD (Create, read, update, delete)
 
 }
