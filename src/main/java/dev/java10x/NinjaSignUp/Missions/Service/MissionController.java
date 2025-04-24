1be889286c965fb2243199b618b8aca02da7b5e9
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-04-24 09:40:25 -0700

    feat: adds crud operations to /MissionController

--- src/main/java/dev/java10x/NinjaSignUp/Missions/Service/MissionController.java
+++ src/main/java/dev/java10x/NinjaSignUp/Missions/Service/MissionController.java
@@ -1,4 +1,39 @@
 package dev.java10x.NinjaSignUp.Missions.Service;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("mission")
 public class MissionController {
+
+    // C Post -- Send req to create missions
+    @PostMapping("/create")
+    public String createMission(){
+        return "create mission";
+    }
+
+    // R Get -- Send req to show missions
+    @GetMapping("/readall")
+    public String showAllMission(){
+        return "show all";
+    }
+
+    @GetMapping("/readID")
+    public String showID(){
+        return "showID";
+    }
+
+    // U Put -- Send req to update a mission
+    @PutMapping("/updateID")
+    public String updateID(){
+        return "update ID";
+    }
+
+    // D Delete -- Send req to delete a mission
+    @DeleteMapping("/deleteID")
+    public String deleteID(){
+        return "delete ID";
+    }
+
+
 }
